e4b49c48d4816206a1fc40b30204928390535794
ErwanKaawar <151039285+ErwanKaawar@users.noreply.github.com>
2023-11-16 16:13:05 +0100

    DeleteObjects
    I have delete some ....

--- pychatbot-KAAWAR-RINAUDO-C/main.py
+++ pychatbot-KAAWAR-RINAUDO-C/main.py
@@ -1,8 +1,6 @@
 from FonctionsProjet import *
 if __name__ == '__main__':
     if __name__ == '__main__':
-        phrase = "Vif juge trempez ce blond whisky aqueux"
-        print(noms_president("Nomination_Sarkozy87.txt"))
+        print(noms_president("Nomination_Sarkozy.txt"))
         print(prenoms("Sarkozy"))
-        print(hello("Erwan"))
 

973f93c6d95a43842baaea47811c8d630c720b3b
ErwanKaawar <151039285+ErwanKaawar@users.noreply.github.com>
2023-11-24 08:52:34 +0100

    Creation

--- pychatbot-KAAWAR-RINAUDO-C/main.py
+++ pychatbot-KAAWAR-RINAUDO-C/main.py
@@ -1,17 +1,6 @@
-import os
-from FonctionsProjet import *
-if __name__ == '__main__':
-    directory = "./speeches"
-    files_names = list_of_files(directory, "txt")
-    print(convertirMin(files_names)) # Crée 8 nouveaux fichiers avec texte minuscule
-    print(Ponctuation(files_names))
-    #print(matriceTFIDF('cleaned'))
-    #print(calculIDF('cleaned'))
-    #matricee=matriceTFIDF('cleaned')
-    #print(motnonimportant('cleaned'))
-    #print(moteleve(matricee))
-    #print(mot_utilise('cleaned',"nation"))
-    #print(nom_fichier('cleaned','Chirac'))
-    #print(mot_repete('cleaned','Chirac'))
-    #print(mot_hormis_nonimportant('cleaned'))
 
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
